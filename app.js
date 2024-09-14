ce779703a946da7fd34f5c9d2e7d71a7965f3991
arun kumar <arun.devpro@gmail.com>
2024-09-14 23:42:20 +0530

    project setup

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 import express from "express";
 const app = express();
-import dotenv from "dotenv";
-dotenv.config();
 
-const port = process.env.PORT;
-
-const server = app.listen(port, () => {
-  console.log("Server listening on port " + port);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import app from "./app.js";
+import db from "./config/db.config.js";
+
+const { PORT: port = 8000 } = process.env;
+
+process.on("SIGINT", async () => {
+  await db.close();
+  console.log("MongoDB connection closed on app termination");
+  process.exit(0);
+});
+
+// connect express app only if connected to db instance
+db.once("open", function () {
+  console.log("Connected to Database", db.host);
+  // Start the Express server
+  const expressServer = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+});
