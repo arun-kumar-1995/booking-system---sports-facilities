ac9f1dd92a3318948fb352388a9add8199020bbf
arun kumar <arun.devpro@gmail.com>
2024-09-15 10:01:52 +0530

    added user models

---
+++ models/user.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const schema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      trim: true,
+    },
+    last_name: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    bookings: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Booking",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", schema);
+
+// define user methods here
+
+// define usefull index here
+export default User;
