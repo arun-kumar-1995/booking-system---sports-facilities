3f2e13542ef35269057d4948467794f27618fcff
arun kumar <arun.devpro@gmail.com>
2024-09-15 22:08:41 +0530

    updated catchasync errfunction for non atomic operation

--- utils/catchAsyncError.utils.js
+++ utils/catchAsyncError.utils.js
@@ -1,25 +1,27 @@
 import mongoose from "mongoose";
 
-const CatchAsyncError = (controller) => async (req, res, next) => {
-  // start mongoose session
-  const session = await mongoose.startSession();
-  // begin the transaction
-  session.startTransaction();
-  try {
-    //catch async error and pass to next middleware
-    // Pass session to controller
-    await Promise.resolve(controller(req, res, next, session));
-    // Commit if all succeeds
-    await session.commitTransaction();
-  } catch (err) {
-    // rollback transaction if error happens
-    await session.abortTransaction();
-    // Pass the error to the next middleware
-    next(err);
-  } finally {
-    // end mongoose session either success or failure
-    session.endSession();
-  }
-};
+const CatchAsyncError =
+  (controller, useTransaction = false) =>
+  async (req, res, next) => {
+    // start mongoose session
+    const session = useTransaction ? await mongoose.startSession() : null;
+    // begin the transaction
+    if(session) session.startTransaction();
+    try {
+      //catch async error and pass to next middleware
+      // Pass session to controller
+      await Promise.resolve(controller(req, res, next, session));
+      // Commit if all succeeds
+      if(session) await session.commitTransaction();
+    } catch (err) {
+      // rollback transaction if error happens
+      if(session) await session.abortTransaction();
+      // Pass the error to the next middleware
+      next(err);
+    } finally {
+      // end mongoose session either success or failure
+      if(session) await session.endSession();
+    }
+  };
 
 export default CatchAsyncError;
