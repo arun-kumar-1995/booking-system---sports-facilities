824f0281026d9ada5e112274c86fa19cda3b4bf0
arun kumar <arun.devpro@gmail.com>
2024-09-15 20:34:09 +0530

    added catchAsync Error inside signup controller

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,20 +1,17 @@
 import User from "../models/user.models.js";
+import CatchAsyncError from "../utils/catchAsyncError.utils.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import sendResponse from "../utils/responseHandler.js";
 
-export const signUp = async (req, res, next) => {
-  try {
-    //find user by email or phone number
-    const user = await User.findOne({
-      $or: [{ email: req.body.email }, { phone: parseInt(req.body.phone) }],
-    });
+export const signUp = CatchAsyncError(async (req, res, next) => {
+  //find user by email or phone number
+  const user = await User.findOne({
+    $or: [{ email: req.body.email }, { phone: parseInt(req.body.phone) }],
+  }).lean();
 
-    if (user) throw new ErrorHandler(409, "user already exists");
+  if (user) throw new ErrorHandler(409, "user already exists");
 
-    // else create a new user
-    await User.create(req.body);
-    sendResponse(res, 201, "User registered");
-  } catch (err) {
-    next(err);
-  }
-};
+  // else create a new user
+  await User.create(req.body);
+  sendResponse(res, 201, "User registered");
+});
