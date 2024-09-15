e278f5130eed0419968c5bf021e9d53c8f978e0f
arun kumar <arun.devpro@gmail.com>
2024-09-15 10:19:17 +0530

    testing user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-
+router.get("/", (req, res, next) => {
+  console.log("route hits");
+  return res.status(200).json({
+    success: true,
+    message: "Testing user route",
+  });
+});
 
 export default router;
