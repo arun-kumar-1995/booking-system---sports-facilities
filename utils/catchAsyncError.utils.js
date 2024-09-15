300b8c676f234bda8ee83d8248a0fb3b83da5cec
arun kumar <arun.devpro@gmail.com>
2024-09-15 21:09:15 +0530

    implementing atomicity in catch async error

--- utils/catchAsyncError.utils.js
+++ utils/catchAsyncError.utils.js
@@ -1,11 +1,24 @@
-const CatchAsyncError = (controller) => (req, res, next) => {
+import mongoose from "mongoose";
+
+const CatchAsyncError = (controller) => async (req, res, next) => {
+  // start mongoose session
+  const session = await mongoose.startSession();
+  // begin the transaction
+  session.startTransaction();
   try {
     //catch async error and pass to next middleware
-    Promise.resolve(controller(req, res, next)).catch(next);
+    // Pass session to controller
+    await Promise.resolve(controller(req, res, next, session));
+    // Commit if all succeeds
+    await session.commitTransaction();
   } catch (err) {
-    // catch synchronous error and pass to next middleware
-    // happerns when this function is invoked
+    // rollback transaction if error happens
+    await session.abortTransaction();
+    // Pass the error to the next middleware
     next(err);
+  } finally {
+    // end mongoose session either success or failure
+    session.endSession();
   }
 };
 
