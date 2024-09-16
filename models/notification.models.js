ddc5e3b5b40909240bce2478086177f2216a9e0d
arun kumar <arun.devpro@gmail.com>
2024-09-16 19:59:21 +0530

    notification model added

---
+++ models/notification.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const schema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [
+        "booking-confirmation",
+        "cancellation",
+        "waitlist",
+        "update",
+        "reminder",
+      ],
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+    readAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", schema);
+
+export default Notification;
